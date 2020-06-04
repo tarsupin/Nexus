06403cdb2a5c6d8b32c48e207c2bea5500eeafde
Tarsupin <tarsupin@gmail.com>
2020-06-04 18:35:54 -0500

    Prepare Snail

--- Nexus/Objects/EnemyLand/Snail.cs
+++ Nexus/Objects/EnemyLand/Snail.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 namespace Nexus.Objects {
 
 	public enum SnailSubType : byte {
-		Snail,
+		Snail = 0,
 	}
 
 	public class Snail : EnemyLand {
@@ -15,15 +15,19 @@ namespace Nexus.Objects {
 		public Snail(RoomScene room, byte subType, FVector pos, Dictionary<string, short> paramList) : base(room, subType, pos, paramList) {
 			this.Meta = Systems.mapper.ObjectMetaData[(byte)ObjectEnum.Snail].meta;
 
-			// Movement
-			this.speed = FInt.Create(1.0);
-
 			// Physics, Collisions, etc.
 			this.physics = new Physics(this);
 			this.physics.SetGravity(FInt.Create(0.5));
+
+			// Sub-Types
+			this.AssignSubType(subType);
+
+			// Speed Handling
+			this.speed = FInt.Create(0.6);
+
 			this.physics.velocity.X = (FInt)(0 - this.speed);
 
-			this.AssignSubType(subType);
+			// Bounds
 			this.AssignBoundsByAtlas(10, 4, -4);
 		}
 
@@ -35,14 +39,35 @@ namespace Nexus.Objects {
 		}
 
 		public override void OnDirectionChange() {
+			this.physics.velocity.X = this.speed * (this.FaceRight ? 1 : -1);
 			this.animate.SetAnimation("Snail/" + (this.FaceRight ? "Right" : "Left"), AnimCycleMap.Cycle3Reverse, 15);
 		}
 
-		public override bool GetJumpedOn(Character character, sbyte bounceStrength = 0) {
+		public override bool GetJumpedOn(Character character, sbyte bounceStrength = 3) {
+			Systems.sounds.shellBoop.Play(0.3f, 0f, 0f);
 			character.BounceUp(this, bounceStrength);
-			Systems.sounds.splat1.Play();
-			// TODO: Snail Disappears; create shell in it's place.
-			return this.Die(DeathResult.Special);
+			return this.ReceiveWound();
+		}
+
+		public override bool ReceiveWound() {
+			return this.Die(DeathResult.Squish);
+		}
+
+		public override bool Die(DeathResult deathType) {
+
+			// Knockouts and TNT can still occur, but squishing will cause other behaviors.
+			if(deathType == DeathResult.Knockout) {
+				return base.Die(deathType);
+			}
+
+			this.Destroy();
+
+			// TODO HIGH PRIORITY: Uncomment once "Shell" item is created.
+			// Create Shell In Snail's Place
+			//Shell shell = new Shell(this.scene, ShellSubType.Red, this.posX, this.posY);
+			//this.scene.AddToObjects(shell);
+
+			return true;
 		}
 
 	}
