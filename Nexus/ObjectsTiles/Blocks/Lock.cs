916cc7ee7455ca2fae225ed5e505642578dc8f66
Tarsupin <tarsupin@gmail.com>
2020-06-06 09:58:49 -0500

    Prepared Locks.

--- Nexus/ObjectsTiles/Blocks/Lock.cs
+++ Nexus/ObjectsTiles/Blocks/Lock.cs
@@ -1,4 +1,5 @@
-﻿using Nexus.GameEngine;
+﻿using Nexus.Engine;
+using Nexus.GameEngine;
 using Nexus.Gameplay;
 
 namespace Nexus.Objects {
@@ -17,10 +18,39 @@ namespace Nexus.Objects {
 		public override bool RunImpact(RoomScene room, DynamicObject actor, ushort gridX, ushort gridY, DirCardinal dir) {
 
 			if(actor is Character) {
-				// TODO: Special Lock Behavior, Unlock it if Character has a key.
+				Character character = (Character)actor;
+
+				// Check if the character possesses a key:
+				bool hadKey = character.trailKeys.RemoveKey();
+
+				// Unlock if Character had a key.
+				if(hadKey) {
+					Systems.sounds.unlock.Play();
+					this.DestroyLockGroup(room, gridX, gridY);
+					return false;
+				}
 			}
 
 			return base.RunImpact(room, actor, gridX, gridY, dir);
+		}
+
+		private void DestroyLockGroup(RoomScene room, ushort gridX, ushort gridY) {
+
+			// Check if the tile exists:
+			byte[] td = room.tilemap.GetTileDataAtGrid(gridX, gridY);
+			if(td == null) { return; }
+
+			// If the tile is a Lock, destroy it, and then run DestroyLockGroup on it.
+			if(td[0] != this.tileId) { return; }
+
+			// Destroy This Lock
+			room.tilemap.ClearMainLayer(gridX, gridY);
+
+			// Destroy all Neighbor BGTaps of the same subtype:
+			this.DestroyLockGroup(room, (ushort)(gridX - 1), gridY);
+			this.DestroyLockGroup(room, (ushort)(gridX + 1), gridY);
+			this.DestroyLockGroup(room, gridX, (ushort)(gridY - 1));
+			this.DestroyLockGroup(room, gridX, (ushort)(gridY + 1));
 		}
 
 		public override void Draw(RoomScene room, byte subType, int posX, int posY) {
