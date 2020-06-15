725f7cd7395c6f3966794258d6e55b08895270f9
Tarsupin <tarsupin@gmail.com>
2020-06-15 09:25:05 -0500

    Prepared Dip, Delay, Fall Platforms.

--- Nexus/Objects/Platforms/PlatformDelay.cs
+++ Nexus/Objects/Platforms/PlatformDelay.cs
@@ -1,11 +1,16 @@
 ﻿using Nexus.Engine;
 using Nexus.GameEngine;
 using Nexus.Gameplay;
+using System;
 using System.Collections.Generic;
 
 namespace Nexus.Objects {
 
 	public class PlatformDelay : Platform {
+
+		private sbyte offset = 0;
+		private int activationFrame = 0;
+		private int releaseFrame = 0;
 
 		public PlatformDelay(RoomScene room, byte subType, FVector pos, Dictionary<string, short> paramList) : base(room, subType, pos, paramList) {
 			this.Meta = Systems.mapper.ObjectMetaData[(byte)ObjectEnum.PlatformDelay].meta;
@@ -16,7 +21,29 @@ namespace Nexus.Objects {
 		}
 
 		public override void ActivatePlatform() {
-			this.physics.SetGravity(FInt.Create(0.1));
+			if(this.activationFrame > 0) { return; }
+			this.activationFrame = (int)Systems.timer.Frame;
+			this.releaseFrame = (int)(Systems.timer.Frame + 60);
+		}
+
+		public override void RunTick() {
+
+			// Check if the delay platform is shaking.
+			if(this.activationFrame > 0) {
+				if(this.releaseFrame > Systems.timer.Frame) {
+					float weight = (float)((Systems.timer.frame60Modulus) * 0.08f);
+					this.offset = (sbyte)Math.Round(Interpolation.EaseBothDir(-1.8f, 1.8f, weight));
+				} else if(releaseFrame == Systems.timer.Frame) {
+					this.physics.SetGravity(FInt.Create(0.1));
+					if(this.offset != 0) { this.offset = 0; }
+				}
+			}
+
+			base.RunTick();
+		}
+
+		public override void Draw(int camX, int camY) {
+			this.Meta.Atlas.Draw(this.SpriteName, posX - camX + this.offset, posY - camY);
 		}
 
 		private void AssignSubType(byte subType) {
--- Nexus/Objects/Platforms/PlatformDip.cs
+++ Nexus/Objects/Platforms/PlatformDip.cs
@@ -7,16 +7,57 @@ namespace Nexus.Objects {
 
 	public class PlatformDip : Platform {
 
+		private int dipWeight = 0;
+		private int yStart = 0;
+
 		public PlatformDip(RoomScene room, byte subType, FVector pos, Dictionary<string, short> paramList) : base(room, subType, pos, paramList) {
 			this.Meta = Systems.mapper.ObjectMetaData[(byte)ObjectEnum.PlatformDip].meta;
 			this.AssignSubType(subType);
 			this.AssignBoundsByAtlas(0, 0, 0, 0);
 
+			this.yStart = this.posY;
 			this.physics.SetGravity(FInt.Create(0));
 		}
 
+		public override void RunTick() {
+
+			// If there is momentum releasing on the dip platform.
+			if(this.dipWeight <= 0) {
+
+				// If the platform is moving (not stationed at its original position):
+				if(this.physics.gravity != 0) {
+
+					// If the platform is below the starting height:
+					if(this.posY > this.yStart) {
+						this.physics.SetGravity(FInt.Create(-0.1));
+					}
+					
+					// If the platform is above (or at) the starting height.
+					else {
+						this.physics.SetGravity(FInt.Create(0));
+						this.physics.velocity.Y = FInt.Create(0);
+						if(this.posY < this.yStart) {
+							this.physics.MoveToPosY(this.yStart);
+							return;
+						}
+					}
+				}
+			}
+
+			// If there is no current momentum on the dip platform, begin resetting the weight.
+			else {
+				this.dipWeight -= 3;
+				this.physics.SetGravity(FInt.Create(0.05));
+			}
+
+			base.RunTick();
+		}
+
 		public override void ActivatePlatform() {
-			this.physics.SetGravity(FInt.Create(0.1));
+			if(this.dipWeight < 60) {
+				if(this.dipWeight < 12) { this.dipWeight = 12; }
+				this.dipWeight += 3;
+			}
 		}
 
 		private void AssignSubType(byte subType) {
