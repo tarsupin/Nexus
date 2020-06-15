5b856f240dbf0aae87b47867413727b4d6999141
Tarsupin <tarsupin@gmail.com>
2020-06-15 09:44:33 -0500

    Prepared Draft of PlatformMove.

--- Nexus/Objects/Platforms/PlatformMove.cs
+++ Nexus/Objects/Platforms/PlatformMove.cs
@@ -1,6 +1,7 @@
 ﻿using Nexus.Engine;
 using Nexus.GameEngine;
 using Nexus.Gameplay;
+using Nexus.ObjectComponents;
 using System.Collections.Generic;
 
 namespace Nexus.Objects {
@@ -9,11 +10,29 @@ namespace Nexus.Objects {
 
 		public bool triggeredByTouch = false;
 		public int startTime = 0;
+		private Behavior behavior;
 
 		public PlatformMove(RoomScene room, byte subType, FVector pos, Dictionary<string, short> paramList) : base(room, subType, pos, paramList) {
 			this.Meta = Systems.mapper.ObjectMetaData[(byte)ObjectEnum.PlatformMove].meta;
 			this.AssignSubType(subType);
 			this.AssignBoundsByAtlas(0, 0, 0, 0);
+
+			// Assign Flight Behavior
+			this.behavior = FlightBehavior.AssignFlightMotion(this, paramList);
+		}
+
+		public override void RunTick() {
+
+			// Flight Behavior
+			this.behavior.RunTick();
+
+			// Standard Physics
+			this.physics.RunPhysicsTick();
+
+			// Animations, if applicable.
+			if(this.animate is Animate) {
+				this.animate.RunAnimationTick(Systems.timer);
+			}
 		}
 
 		public void ActivateMoveTo() {
