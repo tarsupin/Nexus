8ecf309b2a2739b2911f6d79290eac443b88119b
Tarsupin <tarsupin@gmail.com>
2020-06-26 15:05:04 -0500

    Web Launching Functionality.

---
+++ Nexus/Engine/WebHandler.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Nexus.Engine {
+
+	public static class WebHandler {
+
+		public static string LaunchURL(string url) {
+
+			// Attempt to Start the URL Process
+			try {
+				System.Diagnostics.Process.Start(url);
+			}
+			
+			// If there is no browser that opens:
+			catch (System.ComponentModel.Win32Exception exception) {
+				if(exception.ErrorCode == -2147467259) {
+					return exception.Message;
+				}
+			}
+
+			// Other Issue
+			catch(System.Exception other) {
+				return other.Message;
+			}
+
+			return "";
+		}
+	}
+}
